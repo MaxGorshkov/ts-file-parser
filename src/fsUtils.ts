054ed0406bbf80d10b8dcc98b1b282af44de1d13
MGorshkov <mgorshkov@softmedialab.com>
2017-10-02 14:37:09 +0500

    wrong commit

--- src/fsUtils.ts
+++ src/fsUtils.ts
@@ -2,7 +2,7 @@ import * as pth from "path";
 import * as fs from "fs";
 
 
-export function resolve(p1: string, p2: string): string {
+export function resolve(p1: string, p2: string): string{
     return pth.resolve(p1, p2);
 }
 
